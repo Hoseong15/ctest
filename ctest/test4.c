9e34cd624e58569196c1a602d29a08fe47014cb8
Hoseong15 <dlsghtjd3982@gamil.com>
2024-03-06 17:31:04 +0900

    3.6수업

--- ctest/test4.c
+++ ctest/test4.c
@@ -2,5 +2,44 @@
 
 void main() {
 
+	// 1부터 10까지 출력하면서 짝수 출력할떄는 ***을 같이 출력하세요.
+
+	// 반복문 - while, for, do~while
+
+	/*while (1) {
+
+	}*/
+	for (int i = 1; i <= 10; i++) {
+		if (i % 2 == 0) {
+			printf("%d *** \n", i);
+		}
+		else {
+			printf("%d\n", i);
+		}
+		
+	} 
+
+	// 1부터 10까지 반복문을 만들고 출력하는데
+	// 숫자 출력 대신 , 숫자만큼 *을 출력하세요.
+
+	/*for (int i = 1; i <= 10; i++) {
+		for (int a = 1; a <= i; a++) {
+			printf("*");
+		}
+		printf("\n");
+	}*/
+
+	
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= 10 - i; j++) {
+			printf(" ");
+		}
+		for (int k = 1; k <= 2 * i -1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
 
 }
