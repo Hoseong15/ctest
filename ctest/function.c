322f9d5b6d8e722e568079c2be7ea7d61c519a86
Hoseong15 <dlsghtjd3982@gamil.com>
2024-03-07 17:34:14 +0900

    3.7c언어(함수,난수)

---
+++ ctest/function.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+
+
+// 재귀함수 - 자신을 실행시키는 함수
+
+//void out(int num) {
+//	printf("%d\n",num);
+//	if (num < 10) {
+//		out(++num);
+//	}
+//	return;
+//}
+
+
+// 재귀함수로 구구단 4단 출력 해보세요
+//void four(int x, int y) {
+//	printf("%d * %d = %d\n", x,y, x*y);
+//	if (y == 9)return;
+//	four(x, y + 1);
+//}
+
+//int fac(int num) {
+//	if (num == 1) return 1;
+//	return num * fac(num - 1);
+//}
+//
+//int ten(int x) {
+//	if (x == 1) return 1;
+//	return x + ten(x - 1);
+//}
+
+
+
+// 숙제 //
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+// 
+// Sleep(1000);
+// 
+//void draw(); 그림 그리는 함수
+//int game();  게임 시작함수
+//void result(int select); 결과 출력함수
+//
+//void main() {
+//	game();
+//}
+
+
+
+// c에서 함수를 만들때 main위에 만들어야한다.
+
+//void a();
+//void b();
+//void c();
+//void d();
+//void e();
+
+
+// 함수이름은 함수의 기능에 맞는 이름으로정한다. 네이밍표기법에따라
+// 매개변수는 함수의 내용을 실행하는데 외부데이터가 필요한경우 생성한다.
+// return 반환은 함수가 실행되고 나온 결과 값에대해 외부에서 사용해야 되는경우
+// 반환타입 함수이름(매개변수)
+
+//void sum(int a, int b) {
+//	int add = a+b;
+//	printf("합 : %d\n", add);
+//}
+//
+//int total(int k, int e, int m) {
+//	int add = k + e + m;
+//	printf("총점은 : %d\n", add);
+//	return add;
+//}
+//
+//void avg(int tot) {
+//	printf("평균은 %d\n", tot / 3);
+//}
+
+
+//int sum(int x, int y) {
+//	int temp;
+//	temp = x + y;
+//	return temp;
+//}
+
+
+// 정수를 하나 입력한다.
+// 입력한 정수에대해 짝수인지 홀수인지 출력하는 함수를 만들기
+// 단, 0을 입력하면 에러라고 출력되어야한다.
+
+//int pus(int x) {
+//	if (x == 0) {
+//		printf("에러");
+//	}
+//	else if (x % 2 == 0) {
+//		printf("짝수");
+//	}
+//	else {
+//		printf("홀수");
+//	}
+//
+//}
+
+// 999999이하의 숫자를 입력하여 입력한 숫자가 몇자리 숫자인지
+// 구하여 main함수에서 출력하기
+
+//int min(int num) {
+//	int count = 0;
+//	while (num != 0) {
+//		num /= 10;
+//		count++;
+//	}
+//	return count;
+//	
+//
+//}
+
+//double centiMeter(int x) {
+//	double meter = x / 100.0;
+//	return meter;
+//
+//}
+
+//void sum(int count) {
+//	int total = 0;
+//	for (int i = 1; i <= count; i++) {
+//		total+= i;
+//	}
+//
+//	printf("1부터 %d까지 합은 %d입니다\n", count, total);
+//}
+
+
+void main() {
+
+	/*out(1);*/
+
+	/*four(4,1);*/
+
+	/*printf("%d\n" ,fac(5));
+	printf("%d\n", ten(10));*/
+
+	/*int kor = 80, eng = 88, mat = 90;
+	int tot = total(kor, eng, mat);
+
+	avg(tot);
+
+	int a1 = 10, b1 = 20;
+	sum(a1, b1);*/
+
+	//a();
+
+	/*int a = 10, b = 20;
+	int result;
+	result = sum(a, b);
+	printf("결과 : %d", result);*/
+
+	/*int a;
+	printf("정수 입력하세요");
+	scanf_s("%d", &a);
+	pus(a);*/
+
+	/*int c;
+	printf("숫자 입력");
+	scanf_s("%d", &c);
+	int a = min(c);
+	printf("결과 %d은 %d자리수이다.",c, a);*/
+
+	/*double res;
+	res = centiMeter(187);
+	printf("%.2lfm\n", res);*/
+
+	/*sum(10);
+	sum(100);*/
+	
+	
+
+}
+
+
+//void a() {
+//	printf("나는 함수a\n");
+//}
+//void b() {
+//	printf("나는 함수b\n");
+//}
+//void c() {
+//	printf("나는 함수c\n");
+//}
+//void d() {
+//	printf("나는 함수d\n");
+//}
+//void e() {
+//	printf("나는 함수e\n");
+//}
--- ctest/test4.c
+++ ctest/test4.c
@@ -1,15 +1,140 @@
 #include <stdio.h>
+#include <stdlib.h> // 난수
+#include <stdbool.h> // bool
 
 void main() {
 
-	// 1부터 10까지 출력하면서 짝수 출력할떄는 ***을 같이 출력하세요.
+	//srand(time(NULL));
+
+	/*printf("%2d %2d %2d %2d %2d", 1, 20,11,12,13);*/
+
+	/*int num = 0, cnt = 0;   
+	bool isPrime = true;
+	printf("양수입력");
+	scanf_s("%d", &num);
+
+	for (int i = 2; i <= num; i++) {
+		isPrime = true;
+		for (int a = 2; a < i; a++) {  
+			if (i % a == 0)
+				isPrime = false;
+				break;             
+		}
+		if (isPrime == true)
+			printf("%5d", i); cnt++;
+		if (cnt == 5) 
+			printf("\n"); cnt = 0;
+
+	}*/
+
+
+
+
+
+
+
+	// 정수를 계속 입력한다. 입력하는 정수의 범위는 1~50중에서만 입력
+	// 정수를 입력할때 마다 난수를 발생시킨다.
+	// 난수의 범위는 1~9다.
+	// 내가 입력한 정수가 난수의 배수라면 당첨이라고 출력하고 반복문 종료
+
+	/*while (1) {
+		int a = 0;
+		printf("1부터 50 사이의 정수를 입력: ");
+		scanf_s("%d", &a);
+
+		if (a < 1 || a > 50) {
+			printf("1부터 50 사이의 정수만 입력.\n");
+			continue;
+		}
+		int num = rand() % 9 + 1; 
+		printf("난수: %d\n", num);
+
+		if (num != 0 && a % num == 0) {
+			printf("당첨!\n");
+			break;
+		}
+	}*/
+
+	//int num = rand()%10 + 1; 
+	//21 ~ 57
+	//int num = rand()%37 + 21; 
+	//printf("%d", num);
+
+
+
+
+	// 내가 입력한 정수에 맞는 구구단을 출력한다.
+	// 단, 5단을 출력할때는 5*5 = 25 다음에 6*5=30으로 변경되어 출력
+	
+	/*int a = 0;
+	scanf_s("%d", &a);
+	for (int i = 1; i <= 9; i++) {
+		if (a == 5 && i == 6) {
+			int temp = a;
+			a = i;
+			i = temp;
+		}
+		printf("%d * %d = %d\n", a, i, a * i);
+
+		if (a == 5 && i == 5) {
+			a++; i--;
+		}
+	}*/
+	
+
+	// 구구단 2단을 출력하고, 2 * 9 = 18이 마지막줄에 출력되고 나면
+	// 다음줄에 9*2 = 18로 시작하여 9단을 출력하세요
+	// 전체 변수는 총 2개만 사용하세요.
+
+	/*int a = 2;
+	int b = 1;
+	for (; b <= 9; b++) {
+		printf("%d * %d = %d \n", a, b, a * b);
+	}
+	b--;
+	for (; a <= 9; a++) {
+		printf("%d * %d = %d \n", b, a, b * a);
+	}*/
+	
+
+	/*int num = 1;
+
+	while (num !=11) {
+		printf("정수 입력(0-종료) : ");
+		scanf_s("%d", &num);
+	}*/
+
+	/*int num = 0;
+	do {
+		printf("정수 입력(0-종료) : ");
+		scanf_s("%d", &num);
+	} while (num != 10);*/
+	
 
 	// 반복문 - while, for, do~while
 
-	/*while (1) {
+	// while문
+	//	int a = 1; 초기값
+	// while(a <= 5){조건식
+	//	a++; 증감식
+	// }
 
-	}*/
-	for (int i = 1; i <= 10; i++) {
+	// for문
+	// for(int a = 1; a <= 5; i++{초기값; 조건식; 증감식
+	// }
+
+	// do~while문
+	// int a = 1; 초기값
+	// do{
+	//	a++; 증감식
+	// } while (a <= 4);조건식
+
+
+
+	// 1부터 10까지 출력하면서 짝수 출력할떄는 ***을 같이 출력하세요.
+
+	/*for (int i = 1; i <= 10; i++) {
 		if (i % 2 == 0) {
 			printf("%d *** \n", i);
 		}
@@ -17,7 +142,7 @@ void main() {
 			printf("%d\n", i);
 		}
 		
-	} 
+	} */
 
 	// 1부터 10까지 반복문을 만들고 출력하는데
 	// 숫자 출력 대신 , 숫자만큼 *을 출력하세요.
@@ -30,15 +155,15 @@ void main() {
 	}*/
 
 	
-	for (int i = 1; i <= 10; i++) {
-		for (int j = 1; j <= 10 - i; j++) {
+	/*for (int i = 1; i <= 4; i++) {
+		for (int j = 1; j <= 4 - i; j++) {
 			printf(" ");
 		}
 		for (int k = 1; k <= 2 * i -1; k++) {
 			printf("*");
 		}
 		printf("\n");
-	}
+	}*/
 
 
 
